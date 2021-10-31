97fbb7c8f5c1a18b75762ca5e83496f5d5bc684f
KeyboardKatsu <KeyboardKatsu@users.noreply.github.com>
2021-10-31 13:49:03 +0000

    deploy: f907033713ab0942336b84df5b76b5cf839a26f5

---
+++ component---src-pages-index-js-77ae8efc9e0ecdfe2902.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfirst_gatsby=self.webpackChunkfirst_gatsby||[]).push([[678],{629:function(t,e,a){a.r(e),a.d(e,{default:function(){return o}});var n=a(294),c=a.p+"static/logo-512-10c8f459dd6233a53eaa722e79cd344b.png",o=function(){return n.createElement("main",{className:"bg-layout"},n.createElement("a",{href:"https://twitter.com/gavinwoof"},n.createElement("img",{src:c,width:"100%",height:"auto",alt:"Gavin Woof Logo"})),n.createElement("h1",null,"Fun times soon 👀"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-77ae8efc9e0ecdfe2902.js.map
